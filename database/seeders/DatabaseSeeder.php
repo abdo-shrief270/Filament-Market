2f5c3e8a4187ec9474e6ad8aefa7f63c94b2b7c5
abdelrahman shrief ali <abdo.shrief270@gmail.com>
2025-01-07 18:28:19 +0200

    update Db seeder for roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
+        \App\Models\User::create([
+            'name'=>'Abdo Shrief',
+            'phone'=>'01270989676',
+            'email'=>'abdo.shrief270@gmail.com',
+            'type'=>'admin',
+            'password'=>\Illuminate\Support\Facades\Hash::make('12345678'),
+            'active'=>true
+        ]);
+        Artisan::call('shield:install admin');
+        Artisan::call('shield:generate --all --panel=admin');
+        Artisan::call('shield:super-admin --user=1');
+        \Spatie\Permission\Models\Role::create(['name'=>'manager', 'guard_name'=>'web']);
+        \Spatie\Permission\Models\Role::create(['name'=>'courier', 'guard_name'=>'web']);
     }
 }
